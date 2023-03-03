b88ea6c1438f9ba40755ceba7b93dbcd6a7e355e
Ria8651 <bk.dev@icloud.com>
2023-03-03 21:44:13 +1300

    load mc file

--- src/main.rs
+++ src/main.rs
@@ -44,4 +44,38 @@ fn setup(mut commands: Commands) {
             up: Vec3::Y,
         },
     ));
+
+    use fastanvil::{CurrentJavaChunk, Region};
+    use fastnbt::from_bytes;
+
+    let file = std::fs::File::open(
+        "/Users/brian/Desktop/Server stuff/Minecraft/1.18.2 Testing Server/world/region/r.0.0.mca",
+    )
+    .unwrap();
+
+    let mut region = Region::from_stream(file).unwrap();
+    let data = region.read_chunk(0, 0).unwrap().unwrap();
+
+    let chunk: CurrentJavaChunk = from_bytes(data.as_slice()).unwrap();
+    let section_tower = chunk.sections.unwrap();
+    let section = section_tower.get_section_for_y(60).unwrap();
+    let block_states = &section.block_states;
+
+    println!();
+    for x in 0..16 {
+        for y in 0..16 {
+            match block_states.at(x, y, 0) {
+                Some(block) => print!(
+                    "{}",
+                    match block.name() {
+                        "minecraft:stone" => "#",
+                        "minecraft:air" => " ",
+                        _ => "?",
+                    }
+                ),
+                None => print!("."),
+            }
+        }
+        println!();
+    }
 }
